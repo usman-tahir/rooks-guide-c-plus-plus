2fa832d86804953feff4d34fa212a3c9d9ed1ac7
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-05 20:10:17 -0400

    initial commit with chapter 2 exercises

---
+++ chapter_2_exercises.cpp
@@ -0,0 +1,22 @@
+
+int main() {
+  // Declare two variables of type int and initialize them to an appropriate value
+  int x = 10;
+  int y = 20;
+
+  // Declare three integer variables: sum, a, and b. Initializ the variables a and b
+  // to an appropriate integer and use an assignment statement to assign sum the
+  // result of a plus b.
+  int sum, a = 2, b = 2;
+  sum = a + b;
+
+  // Declare a double variable called number and initialize it to 13.6.
+  double number = 13.6;
+
+  // Create a program in which 3 variables are declared. Create one float named myFloat,
+  // one int named myInt, and one double named myDouble. Initialize them to 3.14, 3, and
+  // 3.14159 respectively
+  float myFloat = 3.14;
+  int myInt = 3;
+  double myDouble = 3.14159;
+}
