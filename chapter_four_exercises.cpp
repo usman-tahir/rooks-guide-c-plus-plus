2a700d2d992ada6fc1493047313d26e025583a8d
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-06 10:37:16 -0400

    chapter 4 exercises completed

---
+++ chapter_four_exercises.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Declare two int variables
+  int a = 10;
+  int b = 20;
+
+  // Declare two double variables
+  double c = 10.50;
+  double d = 20.50;
+
+  // Add and subtract 5 from the int variables
+  a += 5;
+  b -= 5;
+
+  // Add and subtract 7.32 from the double variables
+  c += 7.32;
+  d -= 7.32;
+
+  // Output the variables
+  cout << a << '\n';
+  cout << b << '\n';
+  cout << c << '\n';
+  cout << d << '\n';
+}
