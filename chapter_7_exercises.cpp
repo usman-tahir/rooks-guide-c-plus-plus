8282cd6a5fee68b0ca690fc6b2cdcae186ebf884
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-06 11:58:22 -0400

    chapter 7 exercises (completed) with additional snippet

---
+++ chapter_7_exercises.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  a = 6;
+  b = 3;
+
+  int c = 4, d = 2;
+  c = (c + 2) * d;
+  cout << c << endl;
+
+  return 0;
+}
---
+++ chapter_7_operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 5, b = 10, c = 15, d = 20;
+  
+  cout << "a + b = " << (a + b) << endl;
+  cout << "d - c = " << (d - c) << endl;
+  cout << "a * b = " << (a * b) << endl;
+  cout << "d / a = " << (d / a) << endl;
+
+  return 0;
+}
