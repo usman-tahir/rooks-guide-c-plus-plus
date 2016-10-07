816187ec3860f2d2e9e00d60ef7a22d53e019a80
= <=>
2016-10-07 11:33:13 -0400

    commenting for chapter_12_forloop.cpp code

--- chapter_12_forloop.cpp
+++ chapter_12_forloop.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main() {
   int counter;
-
+  // Initialization, guard, incrementor / decrementor
   for (counter = 0; counter < 5; counter++) {
+    // (counter + 1) addresses the zero-indexing convention
     cout << "The for loop counter is currently on loop " << (counter + 1) << " of 5." << endl;
   }
+
   return 0;
 }
