0045259d642419fc1def4d0bfcd8832463f0ec9e
usman-tahir <=>
2016-10-07 11:21:43 -0400

    minor code adjustments to chapter 12 code

--- chapter_12_exercises.cpp
+++ chapter_12_exercises.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 
   // While loop that increments x to 30
-  int x;
+  int x, y = 0, temp = 0;
   
   while (x < 30) {
     x += 3;
@@ -12,16 +12,14 @@ int main() {
   cout << "After being incremented by 3 from 0, x = " << x << endl;
 
   // Read in values until y >= 20
-  int y = 0;
-  int temp = 0;
-
   do {
     cout << "Enter a number to add to y (currently " << y << "):  ";
     cin >> temp;
+    // If the correct data type is not entered
     if (cin.fail()) {
       cout << "You did not enter an integer. " << endl;
     } else {
-      y += temp;
+      y += temp; // Add the temporary value (user input) to y
     }
   } while (y < 20);
   cout << "After adding to y, y = " << y << endl;
