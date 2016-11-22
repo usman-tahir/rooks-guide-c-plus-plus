7fd0009a2119563af4734d23335852ab3b80e0e7
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-11-21 21:01:59 -0500

    added a new directory for C++ examples from programiz.com

---
+++ google-cpp-tutorial/exercises-and-projects/number_evaluation.cpp
@@ -0,0 +1,13 @@
+/**
+ * A program that evaluates a passed in number, applies a mathematical
+ * algorithm to it, and then returns the same number
+ */
+
+#include <iostream>
+using namespace std;
+
+// Get input from the user
+int getUserInput() {
+  int input = -1;
+  
+}
